e312d63f47179b3142acf7a35bf0e89c5eac5225
timka <svintsovtima@gmail.com>
2025-09-23 23:13:09 +0300

    ps fce

--- schemas/authJoiSchema.js
+++ schemas/authJoiSchema.js
@@ -1,26 +1,26 @@
 import Joi from "joi"
 
 export const authRegisterJoi = Joi.object({
-    name: Joi.string().required().message({
+    name: Joi.string().required().messages({
         'string.base': 'name must be typed in string',
         'any.required': 'name is required',
     }),
-    email: Joi.string().required().message({
+    email: Joi.string().required().messages({
         'string.base': 'email must be typed in string',
         'any.required': 'email is required',
     }),
-    password: Joi.string().required().message({
+    password: Joi.string().required().messages({
         'string.base': 'password must be typed in string',
         'any.required': 'password is required',
     })
 })
 
 export const authLoginJoi = Joi.object({
-    email: Joi.string().required().message({
+    email: Joi.string().required().messages({
       'string.base': 'email must be typed in string',
         'any.required': 'email is required',
     }),
-        password: Joi.string().required().message({
+        password: Joi.string().required().messages({
         'string.base': 'password must be typed in string',
         'any.required': 'password is required',
     })
