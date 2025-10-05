e0c879fe6c44708363a378e17e98fcfda0148ce3
timka <svintsovtima@gmail.com>
2025-10-05 21:36:43 +0300

    noob

--- src/constants/patchFrom.js
+++ src/constants/patchFrom.js
@@ -3,3 +3,4 @@ import path from "node:path";
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -6,26 +6,26 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routerConnect.js';
 import cookieParser from "cookie-parser";
 import { UPLOAD_DIR } from './constants/patchFrom.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 export const setupServer = () => {
   const app = express();
   const PORT = process.env.PORT || '3000';
 
-    app.use(cors());
-    app.use(pinoHttp());
-    app.use(express.json());
+  app.use(cors());
+  app.use(pinoHttp());
+  app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-    app.use(notFoundHandler);
-
-    app.use(errorHandler);
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+       console.log(`Server is running on port ${PORT}`);
+  });
 };
